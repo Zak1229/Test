f35a1908e411ee2155796f28053bacba24a69fa6
Zak <zakjturner@gmail.com>
2023-06-16 21:16:38 +0100

    first commit

---
+++ operations.py
@@ -0,0 +1,16 @@
+name='zak'
+print(name)
+##print upper case
+print(name.upper())
+##length of characters
+print(len(name))
+##test for decimals
+print(name.isdecimal())
+
+
+
+
+
+
+
+
---
+++ types_practise.py
@@ -0,0 +1,18 @@
+first_name = 'Zak'
+last_name = 'Turner'
+
+fullname_with_space = first_name + ' ' + last_name
+print(fullname_with_space)
+
+first_name = 'Zak'
+last_name = 'Turner'
+print(first_name, last_name)
+
+my_list = list (first_name + last_name)
+
+print(my_list)
+
+names_dictionary = {'Zak': 'First', 'Turner': 'Last'}
+print(names_dictionary)
+
+
